271a2575e08ef6d7a3d32f13533d5400fe23c531
haitan2408 <haitan28102408@gmail.com>
2023-10-09 09:53:38 +0700

    hai tt up demo ss7

---
+++ ss7/demo_variable.js
@@ -0,0 +1,26 @@
+// function avg() {
+//     var a = 5;
+//     if( a < 6) {
+//         let math = 5;
+//     }
+//     console.log(math);
+//
+//
+// }
+// avg();
+// console.log(math);
+// let a = 5;
+// let a = 6;
+// console.log(a);
+
+// console.log(a);
+//
+// let a = 5;
+
+let a = 5;
+let b = 6;
+
+let c = ++a + ++b + --a + ++b - a--;
+console.log(a);
+console.log(b);
+console.log(c);
